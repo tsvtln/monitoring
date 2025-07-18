e328f1d330b16f92364b99f851e543bb76338f84
TSVTLN <112159858+tsvtln@users.noreply.github.com>
2025-07-18 17:08:29 +0300

    FIX: temp CPU scripts

--- zbx/temp_mc.sh
+++ zbx/temp_mc.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# Average temperature of CPU with multiple cores
 sensors | grep 'temp1' | awk '{gsub(/[^0-9.]/,"",$2); sum+=$2; count++} END {if(count>0) print sum/count}'
--- zbx/temperature.sh
+++ zbx/temperature.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# Temperature of CPU with 1 core (like raspberry pi)
 sensors | grep 'temp1' | awk '{print $2}'
